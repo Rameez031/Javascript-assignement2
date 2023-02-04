df3b8cfa7c3645d11c53d54e65cb4524da70b76d
Rameez_ramzy <121360781+Rameez031@users.noreply.github.com>
2023-02-04 22:39:29 +0500

    Javascript-assignement2
    Java-script starting project

---
+++ app.js/app.js
@@ -0,0 +1,142 @@
+                        // Chapter 2
+//  Question No 1
+
+var username
+
+// Question No 2
+
+var myname = "Rameez-ramzy";
+console.log(myname);
+
+// Question No 3
+
+var message = "Hello world!";
+alert(message);
+
+// Question No 4
+
+var StName = "Rameez-ramzy \n"
+var StAge = "18 year old \n"
+var StSubject = "Web & mobile app development \n"
+
+alert(StName + StAge + StSubject);
+
+// Question No 5
+
+var a = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(a);
+
+// Question no 6
+
+var email = "My email address is sheikhrameez031@gmail.com"
+alert(email);
+
+// Question no 7
+
+var book = "I am trying to learn from the book A smarter way to learn Javascript"
+alert(book);
+
+// Question No 8
+
+var p1 = "Yah! i can write HTML content through JavaScript \n"
+// document.write(p1);
+
+// question No 9
+
+var end="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(end);
+
+                // chapter No 2 complete
+
+                // chapter No 3 started
+
+// Question No 1
+
+var age="i am 18 year old"
+alert(age);
+
+// Question No 2
+
+var number="You have visited this site 14 times"
+alert(number);
+
+// Question No 3
+
+var birthyear="My birth year is 2004"
+// document.write(birthyear);
+
+// Question No 4
+
+var VisitorName="Rameez-ramzy"
+var title="T-shirts"
+var qty="5"
+
+document.write(VisitorName,  "ordered" ,  qty ,  title , "(s)" ,  "on XYZ Clothing store"); 
+
+                        // Chapter No 3 complete
+
+// Question No 1
+
+// var weather = "rainy";
+// ​weather = "sunny";
+// weather;
+
+// Question No 2
+
+// var camelCase = "lowercase word, then uppercase";
+// var dinner2Go = "pizza";
+// var I_AM_HUNGRY = true;
+// var _Hello_ = "what a nice greeting"
+// var $_$ = "money eyes";
+
+                // Illegal veriable Name
+
+// var total% = "78";
+// var 2fast2catch = "bold claim";
+// var function = "false";
+// var class = "easy";
+// var .222h% = "wrong name"
+
+//  Question No 3
+
+document.write("<h1>Rules for naming JS variables</h1>");
+
+var Que_b="<p>Variable names can only contain, numbers, $ and.For example :$my_1stVariable</p>";
+document.write(Que_b);
+
+var Que_c="<p>Variables must begin with a letter, $, or. For example $name, _name or name.</p>";
+document.write(Que_c);
+
+var Que_d="<p>Variable names are case Sensitive.</p>"
+document.write(Que_d);
+
+var Que_e="<p>Variable names should not be JS keyword.</p>"
+document.write(Que_e);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
